2fd4a41e2c03cd4ce63d715dc9ffce62add68959
willgpennell <wpennell48@gmail.com>
2024-05-27 15:21:40 -0230

    quick mockup of keen ui menu

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,15 +1,21 @@
-import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
-import { FaFire, FaPoo } from "react-icons/fa";
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { MdPeopleAlt } from "react-icons/md";
+import { HiDocumentSearch } from "react-icons/hi";
+import { FaDatabase } from "react-icons/fa";
+import { IoIosSettings } from "react-icons/io";
 import SideBarIcon from "./SideBarIcon";
 
 export default function SideBar() {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 shadow-2xl">
-      <SideBarIcon icon={<BsPlus size="28" />} text="Add" />
-      <SideBarIcon icon={<FaFire size="28" />} text="Fire" />
-      <SideBarIcon icon={<FaPoo size="28" />} text="Poop" />
-      <SideBarIcon icon={<BsGearFill size="28" />} text="Settings" />
-      <SideBarIcon icon={<BsFillLightningFill size="28" />} text="Vite" />
+    <div className="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col">
+      <SideBarIcon
+        icon={<TbLayoutDashboardFilled size="40" />}
+        text="Dashboard"
+      />
+      <SideBarIcon icon={<MdPeopleAlt size="40" />} text="Clients" />
+      <SideBarIcon icon={<FaDatabase size="36" />} text="Data" />
+      <SideBarIcon icon={<HiDocumentSearch size="48" />} text="Documents" />
+      <SideBarIcon icon={<IoIosSettings size="48" />} text="Settings" />
     </div>
   );
 }
