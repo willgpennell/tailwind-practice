9378daae1d73c6742818a7a2db3d7d8b1717dcc1
willgpennell <wpennell48@gmail.com>
2024-05-27 13:09:12 -0230

    Hover text on buttons

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,11 +5,11 @@ import SideBarIcon from "./SideBarIcon";
 export default function SideBar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 shadow-2xl">
-      <SideBarIcon icon={<BsPlus size="28" />} />
-      <SideBarIcon icon={<FaFire size="28" />} />
-      <SideBarIcon icon={<FaPoo size="28" />} />
-      <SideBarIcon icon={<BsGearFill size="28" />} />
-      <SideBarIcon icon={<BsFillLightningFill size="28" />} />
+      <SideBarIcon icon={<BsPlus size="28" />} text="Add" />
+      <SideBarIcon icon={<FaFire size="28" />} text="Fire" />
+      <SideBarIcon icon={<FaPoo size="28" />} text="Poop" />
+      <SideBarIcon icon={<BsGearFill size="28" />} text="Settings" />
+      <SideBarIcon icon={<BsFillLightningFill size="28" />} text="Vite" />
     </div>
   );
 }
--- src/components/SideBarIcon.tsx
+++ src/components/SideBarIcon.tsx
@@ -1,7 +1,15 @@
 interface IconProps {
   icon: React.ReactNode;
+  text: string;
 }
 
-export default function SideBarIcon({ icon }: IconProps) {
-  return <div className="sidebar-icon">{icon}</div>;
+export default function SideBarIcon({ icon, text }: IconProps) {
+  return (
+    <>
+      <div className="sidebar-icon group">
+        {icon}
+        <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+      </div>
+    </>
+  );
 }
