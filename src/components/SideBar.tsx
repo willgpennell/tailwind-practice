b81e45e306874d87e7043f3fe79d2eb7bfce004a
willgpennell <wpennell48@gmail.com>
2024-05-27 17:13:34 -0230

    added black bars to button list

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,7 +7,7 @@ import SideBarIcon from "./SideBarIcon";
 
 export default function SideBar() {
   return (
-    <div className="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col">
+    <div className="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col border-black border-2 border-t-0 border-b-0 border-l-0">
       <SideBarIcon
         icon={<TbLayoutDashboardFilled size="40" />}
         text="Dashboard"
