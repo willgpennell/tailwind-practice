b701ec8b7a7b68ce99bab2bd11df0d679bdc5906
willgpennell <wpennell48@gmail.com>
2024-05-27 17:29:57 -0230

    added keen logo

---
+++ src/components/KeenLogo.tsx
@@ -0,0 +1,9 @@
+import keen from "../assets/keen.png";
+
+export default function KeenLogo() {
+  return (
+    <div className="scale-75">
+      <img src={keen} alt="Keen Logo" />
+    </div>
+  );
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,10 +4,12 @@ import { HiDocumentSearch } from "react-icons/hi";
 import { FaDatabase } from "react-icons/fa";
 import { IoIosSettings } from "react-icons/io";
 import SideBarIcon from "./SideBarIcon";
+import KeenLogo from "./KeenLogo";
 
 export default function SideBar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col border-black border-2 border-t-0 border-b-0 border-l-0">
+      <KeenLogo />
       <SideBarIcon
         icon={<TbLayoutDashboardFilled size="40" />}
         text="Dashboard"
