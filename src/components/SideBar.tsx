821430117c07d3fa79d893943b42711e3f5e90de
willgpennell <wpennell48@gmail.com>
2024-05-27 12:24:43 -0230

    styled up more of the nav buttons

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,7 +4,7 @@ import SideBarIcon from "./SideBarIcon";
 
 export default function SideBar() {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-2xl">
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 shadow-2xl">
       <SideBarIcon icon={<BsPlus size="28" />} />
       <SideBarIcon icon={<FaFire size="28" />} />
       <SideBarIcon icon={<FaPoo size="28" />} />
